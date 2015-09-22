d5d0be88422ad7ec27aaf81f383a1ad61857ab55
Eugenius Hei Man Lai <equalsbrackets@gmail.com>
2015-09-22 22:57:28 +1000

    basic game start stop logic

--- js/main.js
+++ js/main.js
@@ -1,88 +1,279 @@
-    var scene, camera, renderer;
-    var geometry, material, mesh;
-    var camera, cameraControl;
+var scene, camera, renderer;
+var geometry, material, mesh;
+var camera, cameraControl;
+var mouse;
 
-    init();
-    animate();
+var scoreBoard = document.querySelector(".score");
+var finalScore = document.querySelector('.final-score');
+var score;
 
-    function init() {
+var player;
+var playerAlive;
+var playerSpeed = 0.6;
+var playerColor = 0x24fe8f;
 
-        if (Detector.webgl) {
-            renderer = new THREE.WebGLRenderer({
-                antialias: true, // to get smoother output
-                preserveDrawingBuffer: true // to allow screenshot
-            });
-            renderer.setClearColor(0xbbbbbb);
-        } else {
-            Detector.addGetWebGLMessage();
-            return true;
-        }
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(renderer.domElement);
+var hitBox = 2.4;
+var enemies = [];
+var enemyColor = 0xef1b3f;
+var enemySpeed = 1.8;
 
+init();
+animate();
 
-        // create a scene
-        scene = new THREE.Scene();
+function init() {
 
-        // put a camera in the scene
-        camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-        camera.position.z = 15;
-        scene.add(camera);
-
-        // create a camera control
-        cameraControls = new THREEx.DragPanControls(camera)
-
-				// transparently support window resize
-				THREEx.WindowResize.bind(renderer, camera);
-
-				// here we add the objects
-        var verticesOfCube = [-1, -1, -1, 1, -1, -1, 1, 1, -1, -1, 1, -1, -1, -1, 1, 1, -1, 1, 1, 1, 1, -1, 1, 1, ];
-
-        var indicesOfFaces = [
-            2, 1, 0, 0, 3, 2,
-            0, 4, 7, 7, 3, 0,
-            0, 1, 5, 5, 4, 0,
-            1, 2, 6, 6, 5, 1,
-            2, 3, 7, 7, 6, 2,
-            4, 5, 6, 6, 7, 4
-        ];
-
-        var geometry = new THREE.PolyhedronGeometry(verticesOfCube, indicesOfFaces, 6, 2);
-        material = new THREE.MeshBasicMaterial({
-            color: 0xe74c3c,
-            shading: THREE.FlatShading, 
-            ambient: 0xffffff, 
-            emissive: 0x000000, 
-            specular: 0x111111, 
-            shininess: 100, 
-            wireframe: true, 
-            wireframeLinewidth: 3.66
+    if (Detector.webgl) {
+        renderer = new THREE.WebGLRenderer({
+            antialias: true, // to get smoother output
+            preserveDrawingBuffer: true // to allow screenshot
         });
-
-        mesh = new THREE.Mesh(geometry, material);
-        scene.add(mesh);
+        renderer.setClearColor(0xbbb4b7);
+    } else {
+        Detector.addGetWebGLMessage();
+        return true;
     }
 
-    // animation loop
-    function animate() {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-        requestAnimationFrame(animate);
+    // setup raycaster
+    raycaster = new THREE.Raycaster();
+    mouse = new THREE.Vector2();
 
-        // do the render
-        render();       
+    // create a scene
+    scene = new THREE.Scene();
+
+    // put a camera in the scene
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.z = 250;
+    scene.add(camera);
+
+    // create a camera control
+    cameraControls = new THREEx.DragPanControls(camera)
+
+    // transparently support window resize
+    THREEx.WindowResize.bind(renderer, camera);
+
+    // Enemy Field
+    obstacleField(250, 1000);
+
+    // player
+    player = player();
+
+    // player movement
+    document.addEventListener('keypress', movementUp, false);
+    document.addEventListener('keypress', movementDown, false);
+    document.addEventListener('keypress', movementLeft, false);
+    document.addEventListener('keypress', movementRight, false);
+
+
+    // check for start game
+    var startButton = document.querySelector(".start-game").childNodes[1]
+    document.addEventListener('click', startGame, false);
+
+}
+
+function startGame() {
+
+    var startScreen = document.querySelector(".start-screen");
+    startScreen.setAttribute("style", "display: none");
+
+    playerAlive = true
+    setInterval(function() {
+        increaseScore();
+    }, 500)
+
+}
+
+function movementUp(event) {
+
+    if (event.keyCode === 119) {
+        // 'w'
+        player.translateY(10)
+    } 
+}
+
+function movementDown(event) {
+
+    if (event.keyCode === 115) {
+        // 's'
+        player.translateY(-10)    
+
+    } 
+}
+
+function movementLeft(event) {
+
+    if (event.keyCode === 97) {
+        // 'a'
+        player.translateX(-10)
+    } 
+}
+
+function movementRight(event) {
+
+    if (event.keyCode === 100) {
+        // 'd'
+        player.translateX(10)        
+
+    }
+}
+
+function player() {
+
+    var geometry = new THREE.BoxGeometry(7, 7, 7),
+        material = new THREE.MeshBasicMaterial( {color: playerColor} );
+
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+    return mesh;
+
+}
+
+function bullet() {
+
+    var geometry = new THREE.BoxGeometry(7, 7, 7),
+        material = new THREE.MeshBasicMaterial( {color: playerColor} );
+
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+    return mesh;    
+
+}
+
+function shootBullet() {
+    var bullet = bullet()
+}
+
+function obstacleField(amount, radius) {
+    // spawn enemies
+    for (var i = 0; i < 250; i++) {
+        var obstacle = this.obstacle(6);
+        obstacle.position.set(
+            radius / 2 - radius * Math.random(),
+            radius / 2 - radius * Math.random(),
+            0.0
+        )
+        enemies.push(obstacle);
+    }
+}
+
+function obstacle(pyramidSideTileSize) {
+    var pyramidGeometry = new THREE.CylinderGeometry( 1, pyramidSideTileSize, pyramidSideTileSize, 4 );
+    var pyramidMaterial = new THREE.MeshBasicMaterial( {color: enemyColor} )
+    pyramidMesh = new THREE.Mesh(pyramidGeometry, pyramidMaterial);
+    pyramidMesh.position.set(-1.5, 0.0, 4.0);
+    scene.add(pyramidMesh);
+
+    return pyramidMesh;
+
+}
+
+function increaseScore(score) {
+
+    score = parseInt(scoreBoard.innerHTML)
+
+    if ( playerAlive === true ) {
+    
+        score += 1
+
+    } else {
+
+        score = 0
+
     }
 
-    function render() {
-				// variable which is increase by Math.PI every seconds - usefull for animation
-				var PIseconds	= Date.now() * Math.PI; 
-				
-				// update camera controls 
-				cameraControls.update();
+    scoreBoard.innerHTML = score
 
-				// animate mesh
-        mesh.rotation.x += 0.001;
-        mesh.rotation.y += 0.002;
-        mesh.rotation.z += 0.003;
+}
 
-        renderer.render(scene, camera);
+function stopScore() {
+
+    scoreBoard.innerHTML = 0
+    score = 0
+
+}
+
+function showFinalScore(score) {
+
+    finalScore.textContent = score;
+
+}
+
+function stopGame() {
+
+    var gameOverScreen = document.querySelector(".game-over");
+    var startAgainButton = document.querySelector(".start-again");
+    var score = scoreBoard.innerHTML;        
+
+        playerAlive = false;
+        gameOverScreen.setAttribute("style", "display: block");
+        startAgainButton.addEventListener('click', function() {
+            gameOverScreen.setAttribute("style", "display: none");
+        }, false);
+
+        showFinalScore(score);
+        stopScore();
+
+}
+
+function scrollEnemies() {
+
+    for (var i = 0; i < enemies.length; i++) {
+        if (enemies[i].position.y < -500) {
+
+            // set position top
+            enemies[i].position.y = 500
+
+        } else {
+
+            // hit detection
+            if (enemies[i].position.distanceTo( player.position ) < 2 * hitBox) {
+                stopGame();
+                console.log("hit!");
+            }
+            // scroll enemies
+            enemies[i].position.y -= enemySpeed;
+            
+            // spin enemies
+            enemies[i].rotation.x += Math.random() * 0.2; 
+            enemies[i].rotation.z += Math.random() * 0.2;
+
+        }
+    }    
+
+}
+
+function destroyEnemies() {
+    enemies.empty
+}
+
+// animation loop
+function animate() {
+
+    requestAnimationFrame(animate);
+
+    // do the render
+    render();
+}
+
+function render() {
+
+    // variable which is increase by Math.PI every seconds - usefull for animation
+    var PIseconds = Date.now() * Math.PI;
+
+    // update camera controls 
+    cameraControls.update();
+
+    // animate player
+    // player.rotation.x += 0.02;
+
+    // animate enemies
+    if (playerAlive == true) {
+
+        scrollEnemies();
+
     }
+
+    renderer.render(scene, camera);
+}
